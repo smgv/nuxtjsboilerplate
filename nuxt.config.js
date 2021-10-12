304f3a07afaf4729b73f6b63e7f463a0c7240650
Vaibhav Mangesh Lokhande <Vaibhav.Lokhande@freecharge.com>
2021-10-12 16:56:24 +0530

    Change port to 8080

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,12 @@
 import colors from "vuetify/es5/util/colors";
 
 export default {
+  // Changing build directory
+  distDir: 'build',
+  target: 'server',
+  server: {
+    port: 8080,
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: "%s - fc-nuxtjs-boilerplate",
