06324fec1ceac1df8ec240d229941ebdcf8c7ea8
Vaibhav Mangesh Lokhande <Vaibhav.Lokhande@freecharge.com>
2021-10-27 17:15:01 +0530

    Added local API

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <h2>Welcome to NuxtJs Boilerplate used to create this project</h2>
     <img :src="image"/>
       <ul >
-   <li v-for="fact in facts" :key="fact.date">
-     {{ fact.name}} on {{ fact.date}} - {{ fact.week_day}}
+   <li>
+    App {{ data.name}} version {{ data.version}} - description {{ data.description}}
    </li>
   </ul>
   </div>
@@ -16,9 +16,9 @@ import fetch from "isomorphic-fetch"
 import logo from "./../assets/axis-bank-logo-vector.png"
 export default {
   async asyncData() {
-    const response = await fetch("https://holidays.abstractapi.com/v1/?api_key=9c9a7f5235ca4da6bfb56952a7ba9c0b&country=IN&year=2020")
-    const facts = await response.json()
-    return  { facts }
+    const response = await fetch("https://leap.api.axisbank.com/version")
+    const  { data } = await response.json()
+    return  { data }
   },
         data () {
             return {
